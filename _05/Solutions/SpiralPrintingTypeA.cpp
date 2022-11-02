3c825aefe7c80fd65ebdcd0dd718ea8b5644b61b
AlexandarBankov <35687825+AlexandarBankov@users.noreply.github.com>
2022-11-02 20:02:36 +0200

    Create SpiralPrintingTypeA.cpp

---
+++ _05/Solutions/SpiralPrintingTypeA.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+    int size;
+    cin >> size;
+
+	for (size_t i = 0; i < size; i++)
+	{
+		for (size_t j = 0; j < size; j++)
+		{
+			cout << (i + i % 2) * size + j + 1 - 2 * (i % 2) * j - (i % 2) << "\t";
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
