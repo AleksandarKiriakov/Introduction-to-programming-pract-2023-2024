3c8637d6e05b269fa25b398462292d0bff13eb5f
ViktorMilanov <101833400+ViktorMilanov@users.noreply.github.com>
2023-10-07 01:28:59 +0300

    Create Task2.cpp

---
+++ Pract.01/Task2.cpp
@@ -0,0 +1,13 @@
+// Task2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+using namespace std;
+#include <iostream>
+
+int main()
+{
+    double a = 5.4;
+    double b = 7.9;
+    double p = 2 * a + 2 * b;
+    double s = a * b;
+    cout << "P = " << p << endl << "S = " <<s<<endl;
+}
