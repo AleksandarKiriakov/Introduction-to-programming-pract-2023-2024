5bc3f5a6433fb950a1fbc1b18b16f1e4cea04c02
ViktorMilanov <101833400+ViktorMilanov@users.noreply.github.com>
2023-10-07 01:28:50 +0300

    Create Task1.cpp

---
+++ Pract.01/Task1.cpp
@@ -0,0 +1,9 @@
+// Task1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+using namespace std;
+#include <iostream>
+
+int main()
+{
+    cout << "Oh what" << endl << "a happy day!" << endl << "Oh yes," << endl << "what a happy day!";
+}
