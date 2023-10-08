4e1826f04cf2803aebf36c5452e6c1dbdaa2af22
Aleksander Valchev <aleksander_val123@abv.bg>
2023-10-08 21:06:25 +0300

    Create Pract-01.cpp

---
+++ Pract-01/Pract-01.cpp
@@ -0,0 +1,242 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// // ---------------Zad 14-------------------------------------------------------------------------
+	// // fast input: 1234123412345678
+	// 
+	//cout << "pract number 14" << endl;
+	//cout << "enter your card number: ";
+	//long long cardnum;
+	//cin >> cardnum;
+
+	//long long mostdigitsofcard = (cardnum / 10000);
+	//cout << mostdigitsofcard << endl;
+	//int lastdigitsofcard = cardnum - mostdigitsofcard * 10000;
+
+	//cout << "xxxxxxxxxxxx" << lastdigitsofcard;
+
+
+
+	// // ---------------Zad 13-------------------------------------------------------------------------
+	// // Fast input: *Not needed*
+	// 
+	//cout << "Pract Number 13" << endl;
+	//int num1, num2;
+	//cout << "Enter the first number: ";
+	//cin >> num1;
+	//cout << "Enter the second number: ";
+	//cin >> num2;
+
+	//cout << (num1 > num2 && cout << num1 << " > " << num2 << endl || 
+	//cout << num1 << " < " << num2 << endl);
+
+
+
+	// // ---------------Zad 12-------------------------------------------------------------------------
+	// // Fast input: 533628
+	// 
+	//cout << "Pract Number 12" << endl;
+	//int seconds;
+	//cout << "Enter seconds: ";
+	//cin >> seconds;
+
+	//int secondsToDaysLeft = ((seconds / 60) / 60) / 24;
+	//int secondsToHoursLeft = ((seconds -
+	//	secondsToDaysLeft * 60 * 60 * 24
+	//	) / 60) / 60;
+	//int secondsToMinutesLeft = (seconds - (
+	//	secondsToHoursLeft * 60 * 60 +
+	//	secondsToDaysLeft * 60 * 60 * 24
+	//	)) / 60;
+	//int secondstoSecondsLeft = seconds - (
+	//	secondsToDaysLeft * 60 * 60 * 24 +
+	//	secondsToHoursLeft * 60 * 60 +
+	//	secondsToMinutesLeft * 60);
+
+	//cout << secondsToDaysLeft << " days, "
+	//	<< secondsToHoursLeft << " hours, "
+	//	<< secondsToMinutesLeft << " minutes, "
+	//	<< secondstoSecondsLeft << " seconds";
+
+
+
+	// // ---------------Zad 11-------------------------------------------------------------------------
+	// // Fast input: *Not needed*
+	//
+	//cout << "Pract Number 11" << endl;
+	//int num1, num2;
+	//cout << "Enter the first number: ";
+	//cin >> num1;
+	//cout << "Enter the second number: ";
+	//cin >> num2;
+
+	//cout << ( num1 > num2 && cout << num1 << endl || 
+	//cout << num2 << endl );
+
+
+
+	// // ---------------Zad 10-------------------------------------------------------------------------
+	// // Fast input: *Not needed*
+	//
+	//cout << "Pract Number 10" << endl;
+	//int num1, num2;
+	//cout << "Enter value for x1: ";
+	//cin >> num1;
+	//cout << "Enter value for x2: ";
+	//cin >> num2;
+
+	//int reverseValueHelper = num1;
+	//num1 = num2;
+	//num2 = reverseValueHelper;
+
+	//cout << "x1 = " << num1 << endl
+	//	<< "x2 = " << num2;
+
+
+
+	// // ---------------Zad 9--------------------------------------------------------------------------
+	// // Fast input: 1 /n		-5 /n		6
+	//
+	//cout << "Pract Number 9" << endl;
+	//int num1, num2, num3;
+	//cout << "Enter value for the first variable (a) : ";
+	//cin >> num1;
+	//cout << "Enter value for the second bariable (b) : ";
+	//cin >> num2;
+	//cout << "Enter value for the third variable (c) : ";
+	//cin >> num3;
+
+	//int d = num2 * num2 - 4 * num1 * num3;
+	//int firstRoot = ( - num2 + sqrt(d) ) / (2 * num1);
+	//int secondRoot = (-num2 - sqrt(d)) / (2 * num1);
+	//
+	//cout << "x1 = " << firstRoot << endl
+	//	<< "x2 = " << secondRoot;
+
+
+
+	// // ---------------Zad 8--------------------------------------------------------------------------
+	// // Fast input: *Not needed*
+	//
+	//cout << "Pract Number 8" << endl;
+	//int radius;
+	//cout << "Enter radius of a circle: ";
+	//cin >> radius;
+
+	//float pi = 3.14159265;
+	//float circumference = 2 * pi * radius;
+	//float surface = pi * radius * radius;
+
+	//cout << "Circumference = " << circumference << endl
+	//	<< "Surface = " << surface;
+
+
+
+
+	// // ---------------Zad 7--------------------------------------------------------------------------
+	// // Fast input: *Not needed*
+	//
+	//cout << "Pract Number 7" << endl;
+	//int numberOfApples, numberOfPears, numberOfBananas;
+	//cout << "Enter number of apples: ";
+	//cin >> numberOfApples;
+	//cout << "Enter number of pears: ";
+	//cin >> numberOfPears;
+	//cout << "Enter number of bananas: ";
+	//cin >> numberOfBananas;
+
+	//cout << "Pesho, don't forget to buy "
+	//	<< numberOfApples << " apples, "
+	//	<< numberOfPears << " pears and "
+	//	<< numberOfBananas << " bananas!";
+
+
+
+	// // ---------------Zad 6--------------------------------------------------------------------------
+	// // Fast input: *Not needed*
+	//
+	//cout << "Pract Number 6" << endl;
+	//int dividend, divisor;
+	//cout << "Enter value for dividend: ";
+	//cin >> dividend;
+	//cout << "Enter value for divisor: ";
+	//cin >> divisor;
+
+	//int quotient = dividend / divisor;
+	//int remainder = dividend % divisor;
+
+	//cout << "The quotient of the division is: " << quotient << endl
+	//	<< "The remainder of the division is: " << remainder;
+
+
+
+	// // ---------------Zad5--------------------------------------------------------------------------
+	// // Fast input: *Not needed*
+	//
+	//cout << "Pract Number 5" << endl;
+	//int num1, num2, num3;
+	//cout << "Enter value for the first number: ";
+	//cin >> num1;
+	//cout << "Enter value for the second number: ";
+	//cin >> num2;
+	//cout << "Enter value for the third number: ";
+	//cin >> num3;
+
+	//cout << ( num1 > num2 && cout << num1 << " > " << num2 << " > " << num3 << endl
+	//	|| cout << num1 << " < " << num2 << " < " << num3 << endl );
+
+
+
+	// // ---------------Zad 4--------------------------------------------------------------------------
+	// // Fast input: *Not needed*
+	//
+	//cout << "Pract Number 4" << endl;
+	//int a, b;
+	//cout << "Please enter the length of the first side: ";
+	//cin >> a;
+	//cout << "Please enter the length of the second: ";
+	//cin >> b;
+
+	//int perimeter = (a + b) * 2;
+	//int surface = a * b;
+
+	//cout << "Perimeter = " << perimeter << endl
+	//	<< "Surface = " << surface;
+
+
+
+	// // ---------------Zad 3--------------------------------------------------------------------------
+	// // Fast input: *Not needed*
+	//
+	//cout << "Pract Number 3" << endl;
+	//float lv;
+	//cout << "Enter lv: ";
+	//cin >> lv;
+	//float lvToDollarsSum = lv * 0.54;
+	//float lvToEuroSum = lv * 0.51;
+
+	//cout << lv << "lv dollars: " << lvToDollarsSum << endl
+	//	<< lv << "lv to euro: " << lvToEuroSum;
+
+
+
+	// // ---------------Zad 2--------------------------------------------------------------------------
+	// // Fast input: *Not needed*
+	//
+	//cout << "Pract Number 2" << endl;
+	//float a = 5.4;
+	//float b = 7.9;
+	//float perimeter = (a + b) * 2;
+	//cout << "P = " << perimeter;
+
+
+	// // ---------------Zad 1--------------------------------------------------------------------------
+	// // Fast input: *Not needed*
+	//
+	//cout << "Pract Number 1" << endl;
+	//cout << "Oh what" << endl
+	//	<< "a happy day!" << endl
+	//	<< "Oh yes," << endl
+	//	<<"what a happy day!";
+}
