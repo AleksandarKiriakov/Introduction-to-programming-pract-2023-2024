9c1990c8d1f9ffbf27246da677a5b66a47adf8e7
KristinaKanteva <kristinakanteva@gmail.com>
2023-10-11 19:44:29 +0300

    Solution for task2

---
+++ Pract.01/Task02.cpp
@@ -0,0 +1,24 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Enter a:" << endl;
+    double a;
+    cin >> a ;
+    cout << "Enter b:" <<endl;
+    double b;
+    cin >> b ;
+    double p;
+    p = 2 * a + 2 * b;
+    cout << "p=" << p << endl;
+    double s;
+    s = a * b;
+    cout << "s=" << s<<endl;
+
+
+}
+
+
