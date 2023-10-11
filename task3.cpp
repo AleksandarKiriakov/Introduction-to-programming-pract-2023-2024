f06268a13344cfd63a8fe50d0cb934cb559eff3d
georgistan <georgi.i.stanachkov@gmail.com>
2023-10-11 20:20:54 +0300

    Made the requested changes to task 3

--- task3.cpp
+++ task3.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 
 int main() {
-	float leva;
+	double leva;
 	cin >> leva;
 
 	cout << "The sum in euro: " << leva / 1.955 << endl;
-	cout << "The sum in dollars: " << leva * 1.846 << endl;
+	cout << "The sum in dollars: " << leva / 1.846 << endl;
 	return 0;
 }
