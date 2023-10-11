5a11d224510f318661fe841cc4df847c23c18ddc
KristinaKanteva <kristinakanteva@gmail.com>
2023-10-11 21:03:24 +0300

    Solution for task13

---
+++ Pract.01/Task13.cpp
@@ -0,0 +1,17 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    cin >> a;
+    int b;
+    cin >> b;
+    cout << a << "<" << b << endl;
+    cout << a << ">" << b << endl;
+    bool areNumbersDifferent = a != b;
+    cout << boolalpha << areNumbersDifferent;
+}
+
+
