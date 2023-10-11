700ebcd64cefb107f53181330f3bd5187d6c4e69
KristinaKanteva <kristinakanteva@gmail.com>
2023-10-11 20:35:56 +0300

    Solution for task11

---
+++ Pract.01/Task11.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a;
+	cin >> a;
+	int b;
+	cin>>b;
+	int min = a < b ? a : b;
+	int max = a < b ? b : a;
+    cout << "min=" << min << endl;
+	cout << "max=" << max << endl;
+
+}
+
