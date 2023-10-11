bfa5acb43b1ea90b82feb947669275678678a18d
KristinaKanteva <kristinakanteva@gmail.com>
2023-10-11 20:04:26 +0300

    Solution for task8

---
+++ Pract.01/Task08.cpp
@@ -0,0 +1,17 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Enter r: "<< endl;
+    double r;
+    cin >> r;
+    double PI = 3.14159265;
+    double C;
+    C = 2 * PI * r;
+    cout <<"C="<<C<<endl;
+    double S;
+    S = PI * r * r;
+    cout << "S=" << S;
+}
+
