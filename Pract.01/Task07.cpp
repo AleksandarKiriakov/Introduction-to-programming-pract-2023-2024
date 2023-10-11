d540096735e0fbebef24e6b524691abaf7dae18a
Zhozemir <joji013@abv.bg>
2023-10-11 22:07:03 +0300

    Task 07

---
+++ Pract.01/Task07.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+	cout << "Apples: ";
+	int Apples;
+	cin >> Apples;
+
+	cout << "Pears: ";
+	int Pears;
+	cin >> Pears;
+
+	cout << "Bananas: ";
+	int Bananas;
+	cin >> Bananas;
+
+	cout << "Pesho, don't forget to buy " << Apples << " apples, " << Pears << " pears and " << Bananas << " bananas!";
+
+}
