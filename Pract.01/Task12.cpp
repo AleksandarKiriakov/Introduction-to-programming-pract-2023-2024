8e70c4f128f1dca75fa9e242bef6a414349b86a5
KristinaKanteva <kristinakanteva@gmail.com>
2023-10-11 20:24:43 +0300

    Solution for task12

---
+++ Pract.01/Task12.cpp
@@ -0,0 +1,31 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Seconds: ";
+    int numberOfSeconds = 0;
+    cin >> numberOfSeconds;
+
+    int days;
+    days = numberOfSeconds / 86400;
+    numberOfSeconds = numberOfSeconds % 86400;
+    cout << days << " days ";
+
+    int hours;
+    hours = numberOfSeconds / 3600;
+    numberOfSeconds = numberOfSeconds % 3600;
+    cout << hours << " hours ";
+
+    int minutes;
+    minutes = numberOfSeconds / 60;
+    numberOfSeconds = numberOfSeconds % 60;
+    cout << minutes << " minutes ";
+
+    int seconds;
+    seconds = numberOfSeconds % 60;
+    cout << seconds << " seconds ";
+}
+
+
