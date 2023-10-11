79ea8aed099e3610667884da40da32baae6b9044
KristinaKanteva <kristinakanteva@gmail.com>
2023-10-11 20:04:57 +0300

    Solution for task9

---
+++ Pract.01/Task09.cpp
@@ -0,0 +1,26 @@
+﻿
+#include <iostream>
+using namespace std;
+#include <math.h>;
+
+int main()
+{
+    cout << "a:";
+    int a;
+    cin >> a;
+    cout << "b:";
+    int b;
+    cin >> b;
+    cout << "c:" ;
+    int c;
+    cin >> c;
+    int D;
+    D = b * b - 4 * a * c;
+    int x1 = (-b + sqrt(D))/ 2;
+    int x2 = (-b - sqrt(D)) / 2;
+    cout << "x1= "<<x1 << endl;
+    cout << "x2= " << x2;
+    
+    
+}
+
