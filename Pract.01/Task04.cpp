6d012a28dd4dda59c7fa9082602b75111422b63c
KristinaKanteva <kristinakanteva@gmail.com>
2023-10-11 19:59:44 +0300

    Solution for task4

---
+++ Pract.01/Task04.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Please enter the lenght of the first side:"<<endl;
+    double a;
+    cin >> a;
+    cout << "Please enter the lenght of the second side"<<endl;
+    double b;
+    cin >> b;
+    double p;
+    p = a * 2 + b * 2;
+    cout << "p=" << p<< endl;
+    double s;
+    s = a * b;
+    cout << "s=" << s;
+
+}
+
