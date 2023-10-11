1db7add0cdc967911b9da1de2a158e819e4b802c
KristinaKanteva <kristinakanteva@gmail.com>
2023-10-11 20:08:19 +0300

    Solution for task10

---
+++ Pract.01/Task10.cpp
@@ -0,0 +1,19 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "a:";
+	int a;
+	cin >> a;
+	cout << "b:";
+	int b;
+	cin >> b;
+	int c = a;
+	a = b;
+	b = c;
+	cout << "a=" << a << endl <<"b=" << b;
+
+}
