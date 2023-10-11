298736a3b0197add2a5bb8c6c6aeab76d36b4928
KristinaKanteva <kristinakanteva@gmail.com>
2023-10-11 20:00:23 +0300

    Solution for task5

---
+++ Pract.01/Task05.cpp
@@ -0,0 +1,17 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double num;
+    cin >> num;
+    double num1;
+    cin >> num1;
+    double num2;
+    cin >> num2;
+    bool isAscending = num < num1 && num1<num2;
+    cout << boolalpha << isAscending;
+}
+
