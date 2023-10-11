bc5fc647f761b6a7f4be1ff18851b835697e45ad
KristinaKanteva <kristinakanteva@gmail.com>
2023-10-11 20:00:44 +0300

    Solution for task6

---
+++ Pract.01/Task06.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Dividend: " << endl;
+    int a;
+    cin >> a;
+    cout << "Devisor: " << endl;
+    int b;
+    cin >> b;
+    cout << "The quotient of the devision is: " << a / b << endl;
+    cout << "The remainder of the devision is: "<<a % b;
+}
+
