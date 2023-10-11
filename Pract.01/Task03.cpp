b8328f99c4e25535214cc367ee935b45bbf773d3
KristinaKanteva <kristinakanteva@gmail.com>
2023-10-11 19:44:50 +0300

    Solution for task3

---
+++ Pract.01/Task03.cpp
@@ -0,0 +1,15 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Enter BGN:" << endl;
+    double num;
+    cin >> num;
+    cout << "EUR: " << num / 1.96 <<endl;
+    cout << "USD: " << num / 1.84;
+   
+
+}
+
