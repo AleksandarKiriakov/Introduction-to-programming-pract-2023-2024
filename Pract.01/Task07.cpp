e2e164dc8fdf41676e3ef6e7dd8d3f587a2a3bb6
KristinaKanteva <kristinakanteva@gmail.com>
2023-10-11 20:03:57 +0300

    Solution for task7

---
+++ Pract.01/Task07.cpp
@@ -0,0 +1,17 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Apples: ";
+	int num;
+	cin >> num;
+	cout << "Pears: ";
+	int num1;
+	cin >> num1;
+	cout << "Bananas: ";
+	int num2;
+	cin >> num2;
+	cout << "Pesho don't forget to buy " << num << " apples," << num1 << " pears and "<<  num2 <<" bananas!";
+}
