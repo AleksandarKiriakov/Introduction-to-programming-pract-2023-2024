393777b4cfc95abc2af1d0c10a03b2a38a73bf13
Zhozemir <joji013@abv.bg>
2023-10-11 22:01:26 +0300

    Task 03

---
+++ Pract.01/Task03.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+
+	double lev;
+	cin >> lev;
+
+	double dollar = lev * 0.54;
+	double euro = lev * 0.51;
+
+
+	cout << lev << " Leva is " << dollar << " dolars " << endl;
+	cout << lev << " Leva is " << euro << " euro ";
+
+}
