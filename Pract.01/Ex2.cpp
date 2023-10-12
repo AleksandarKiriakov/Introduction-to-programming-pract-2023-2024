fd7d3067ca35ef3bada5632c0f3311e9b4234828
Filiz <97398329+itsfilizz@users.noreply.github.com>
2023-10-12 11:49:50 +0300

    Create Ex2.cpp

---
+++ Pract.01/Ex2.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+
+int main()
+{
+    double a = 5.4;
+    double b = 7.9;
+    double s, p;
+
+    s = a * b;
+    p = (a + b) / 2;
+
+    std::cout << "S: " << s;
+    std::cout << "P " << p;
+}
